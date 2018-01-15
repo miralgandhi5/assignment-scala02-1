9e3b7bebdc4d9aada328f638f0d74377d374dbc2
miralgandhi5 <miralgandhi5@gmail.com>
2018-01-15 18:14:17 +0530

    Initial commit

---
+++ src/main/scala/edu/knoldus/Application.scala
@@ -0,0 +1,28 @@
+package edu.knoldus
+
+import org.apache.log4j.Logger
+
+object Application extends App {
+
+ val log = Logger.getLogger(this.getClass)
+
+ val firstMember = 10
+ val secondMember = 20
+ val thirdMember = 30
+ val fourthMember = 40
+ val firstMemberSec = 10
+ val firstMemberThird = 10
+ val thirdMemberSec = 30
+ val fourthMemberSec = 40
+ val times = 2
+ val inputListOne = (19 to 22).toList
+ val inputListTwo = List(firstMember,firstMemberSec,firstMemberThird,secondMember,thirdMember,thirdMemberSec,fourthMember,fourthMemberSec)
+ log.info(s" List before rotation $inputListOne \n ")
+ val outputList = Operations.rotateNPlaces(inputListOne,times)
+ log.info(s" List after rotation $outputList \n ")
+    val result: List[String] = Operations.happyOrSadNum(inputListTwo)
+   result foreach print
+ log.info(s" List before removing consecutive duplicates $inputListTwo \n ")
+ log.info(s" List after removing duplicates ${Operations.removeConsecutive(inputListTwo)} \n ")
+
+}
---
+++ src/main/scala/edu/knoldus/Operations.scala
@@ -0,0 +1,95 @@
+package edu.knoldus
+
+object Operations {
+
+  def runLength(list: List[Int]): List[(Int,Int)] = {
+
+    def innerRun(input: List[Int],output:List[(Int,Int)]): List[(Int,Int)]= {
+      input match {
+        case Nil => output
+        case head :: _ => val res = input.span( _ == head)
+          innerRun(res._2, output :+ (head,res._1.length))
+      }
+    }
+    innerRun(list,List.empty[(Int,Int)])
+  }
+
+  def removeConsecutive(list: List[Int]): List[Int] ={
+
+    def innerRemoveConsecutive(input: List[Int],output:List[Int]): List[Int]= {
+      input match {
+        case Nil => output
+        case head :: head2 :: tail if head == head2 =>
+          innerRemoveConsecutive(head2 :: tail,output)
+        case head :: tail => innerRemoveConsecutive(tail,output :+ head)
+      }
+    }
+    innerRemoveConsecutive(list,List.empty[Int])
+  }
+
+  def rotateNPlaces(list: List[Int],times: Int): List[Int] = {
+
+    def rotateOnce(input: List[Int],current: Int): List[Int] = {
+      input match {
+        case _ :: _ if current == 0 => input
+        case head :: tail => rotateOnce(tail :+ head,current - 1)
+        case Nil => list
+      }
+    }
+    if(times > 0)
+      {
+        rotateOnce(list,times)
+      }
+    else {
+      list
+    }
+
+  }
+
+  def happyOrSadNum(list: List[Int]): List[String] ={
+
+    def checkNum(value: Int ,current: Int,outputList: List[Int]): String = {
+
+      val res: Int = sumOfSquare(current,0)
+
+
+      if(isContains(outputList,res))
+        {
+          s"$value is sad number"
+        }
+      else if(res == 1)
+      {
+        s"$value is happy number"
+      }
+      else
+        {
+          checkNum(value,res,outputList :+ res)
+        }
+    }
+    for (ele <- list) yield checkNum(ele,ele,List.empty[Int])
+  }
+
+  private def sumOfSquare(value: Int,res: Int): Int = {
+    if(value != 0){
+      sumOfSquare(value / 10 ,res + ((value % 10) * (value % 10)))
+    }
+    else {
+      res
+    }
+  }
+
+  private def isContains(list: List[Int],value: Int): Boolean = {
+
+    def checkNumber(input: List[Int]): Boolean = {
+      input match {
+        case Nil => false
+        case head :: _ if head == value => true
+        case _ :: tail => checkNumber(tail)
+      }
+    }
+    checkNumber(list)
+  }
+
+
+
+}
