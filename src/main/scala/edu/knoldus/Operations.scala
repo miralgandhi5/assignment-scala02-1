a6ddd6e008ad1130d511368d00fde77be19d99cf
miralgandhi5 <miralgandhi5@gmail.com>
2018-01-15 18:23:25 +0530

    update in main function

--- src/main/scala/edu/knoldus/Operations.scala
+++ src/main/scala/edu/knoldus/Operations.scala
@@ -2,83 +2,83 @@ package edu.knoldus
 
 object Operations {
 
-  def runLength(list: List[Int]): List[(Int,Int)] = {
+  def runLength(list: List[Int]): List[(Int, Int)] = {
 
-    def innerRun(input: List[Int],output:List[(Int,Int)]): List[(Int,Int)]= {
+    def innerRun(input: List[Int], output: List[(Int, Int)]): List[(Int, Int)] = {
       input match {
         case Nil => output
-        case head :: _ => val res = input.span( _ == head)
-          innerRun(res._2, output :+ (head,res._1.length))
+        case head :: _ => val res = input.span(_ == head)
+          innerRun(res._2, output :+ (head, res._1.length))
       }
     }
-    innerRun(list,List.empty[(Int,Int)])
+
+    innerRun(list, List.empty[(Int, Int)])
   }
 
-  def removeConsecutive(list: List[Int]): List[Int] ={
+  def removeConsecutive(list: List[Int]): List[Int] = {
 
-    def innerRemoveConsecutive(input: List[Int],output:List[Int]): List[Int]= {
+    def innerRemoveConsecutive(input: List[Int], output: List[Int]): List[Int] = {
       input match {
         case Nil => output
         case head :: head2 :: tail if head == head2 =>
-          innerRemoveConsecutive(head2 :: tail,output)
-        case head :: tail => innerRemoveConsecutive(tail,output :+ head)
+          innerRemoveConsecutive(head2 :: tail, output)
+        case head :: tail => innerRemoveConsecutive(tail, output :+ head)
       }
     }
-    innerRemoveConsecutive(list,List.empty[Int])
+
+    innerRemoveConsecutive(list, List.empty[Int])
   }
 
-  def rotateNPlaces(list: List[Int],times: Int): List[Int] = {
+  def rotateNPlaces(list: List[Int], times: Int): List[Int] = {
 
-    def rotateOnce(input: List[Int],current: Int): List[Int] = {
+    def rotateOnce(input: List[Int], current: Int): List[Int] = {
       input match {
         case _ :: _ if current == 0 => input
-        case head :: tail => rotateOnce(tail :+ head,current - 1)
+        case head :: tail => rotateOnce(tail :+ head, current - 1)
         case Nil => list
       }
     }
-    if(times > 0)
-      {
-        rotateOnce(list,times)
-      }
+
+    if (times > 0) {
+      rotateOnce(list, times)
+    }
     else {
       list
     }
 
   }
 
-  def happyOrSadNum(list: List[Int]): List[String] ={
+  def happyOrSadNum(list: List[Int]): List[String] = {
 
-    def checkNum(value: Int ,current: Int,outputList: List[Int]): String = {
+    def checkNum(value: Int, current: Int, outputList: List[Int]): String = {
 
-      val res: Int = sumOfSquare(current,0)
+      val res: Int = sumOfSquare(current, 0)
 
 
-      if(isContains(outputList,res))
-        {
-          s"$value is sad number"
-        }
-      else if(res == 1)
-      {
-        s"$value is happy number"
+      if (isContains(outputList, res)) {
+        s"$value is sad number\n"
       }
-      else
-        {
-          checkNum(value,res,outputList :+ res)
-        }
+      else if (res == 1) {
+        s"$value is happy number\n"
+      }
+      else {
+        checkNum(value, res, outputList :+ res)
+      }
     }
-    for (ele <- list) yield checkNum(ele,ele,List.empty[Int])
+
+    for (ele <- list) yield checkNum(ele, ele, List.empty[Int])
   }
 
-  private def sumOfSquare(value: Int,res: Int): Int = {
-    if(value != 0){
-      sumOfSquare(value / 10 ,res + ((value % 10) * (value % 10)))
+  private def sumOfSquare(value: Int, res: Int): Int = {
+    if (value != 0) {
+      sumOfSquare(value / 10, res + ((value % 10) * (value % 10)))
     }
     else {
       res
     }
   }
 
-  private def isContains(list: List[Int],value: Int): Boolean = {
+  private def isContains(list: List[Int], value: Int): Boolean = {
 
     def checkNumber(input: List[Int]): Boolean = {
       input match {
@@ -87,9 +87,9 @@ object Operations {
         case _ :: tail => checkNumber(tail)
       }
     }
+
     checkNumber(list)
   }
-
 
 
 }
